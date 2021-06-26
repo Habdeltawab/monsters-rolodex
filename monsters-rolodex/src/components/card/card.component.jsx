74f57e666b03180b9222331addb1a2b4f830e135
Hosam <habdeltawab@qci.com>
2021-06-26 02:12:07 -0500

    ESLint Stuff

--- monsters-rolodex/src/components/card/card.component.jsx
+++ monsters-rolodex/src/components/card/card.component.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './card.styles.css';
 
-const Card = (props) => {
-  return (
-    <div className='card-container'>
-      <img
-        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
-        alt='monsterImage'
-      />
-      <h2>{props.monster.name}</h2>
-      <p>{props.monster.email}</p>
-    </div>
-  );
-};
+const Card = (props) => (
+  <div className='card-container'>
+    <img
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      alt='monsterImage'
+    />
+    <h2>{props.monster.name}</h2>
+    <p>{props.monster.email}</p>
+  </div>
+);
 
 export default Card;
